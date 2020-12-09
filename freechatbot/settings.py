6366324ffb71ab38217926d6221035e58858be51
wongfree@gmail.com <wongfree@gmail.com>
2020-12-09 14:56:41 +0800

    restructure

--- freechatbot/settings.py
+++ freechatbot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 FILE_UPLOAD_MAX_MEMORY_SIZE = 52428800
 
+django_heroku.settings(locals())
