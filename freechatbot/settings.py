badaedfa78b1b4964dfca335bd15eba89be341e3
wongfree@gmail.com <wongfree@gmail.com>
2020-12-09 15:23:03 +0800

    restructure

--- freechatbot/settings.py
+++ freechatbot/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
