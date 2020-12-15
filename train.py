bf5910b4dbcc8b81af2cf344c4aa8622aa91ff71
wongfree@gmail.com <wongfree@gmail.com>
2020-12-15 23:39:21 +0800

    upload trained db

--- train.py
+++ train.py
@@ -4,31 +4,31 @@ from freechatbot import settings
 
 chatbot = ChatBot(**settings.CHATTERBOT)
 
-# trainer = ChatterBotCorpusTrainer(chatbot)
-# trainer.train(
-#                 "chatterbot.corpus.english",
-#
-#               )
-# from chatterbot.trainers import ListTrainer
-#
-# trainer = ListTrainer(chatbot)
-#
-# trainer.train([
-#     "How are you?",
-#     "I am good.",
-#     "That is good to hear.",
-#     "Thank you",
-#     "You are welcome.",
-#     "What's your name?",
-#     "My name is Chatterbot",
-#     "know your name",
-#     "My name is Chatterbot",
-#     ])
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train(
+                "chatterbot.corpus.english",
+
+              )
+from chatterbot.trainers import ListTrainer
+
+trainer = ListTrainer(chatbot)
+
+trainer.train([
+    "How are you?",
+    "I am good.",
+    "That is good to hear.",
+    "Thank you",
+    "You are welcome.",
+    "What's your name?",
+    "My name is Chatterbot",
+    "know your name",
+    "My name is Chatterbot",
+    ])
 
 from chatterbot.trainers import UbuntuCorpusTrainer
 
 
-trainer = UbuntuCorpusTrainer(chatbot)
-
-# Start by training our bot with the Ubuntu corpus data
-trainer.train()
+# trainer = UbuntuCorpusTrainer(chatbot)
+#
+# # Start by training our bot with the Ubuntu corpus data
+# trainer.train()
