8aa2e0bbf9d9f5d85d3169b4d4256c4fb7c5d0c5
wongfree@gmail.com <wongfree@gmail.com>
2021-01-05 17:16:42 +0800

    added back task.sh

---
+++ bot/views/report.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import sqlite3
+
+
+db = create_engine('sqlite:///text.sqlite3')
+
+
--- freechatbot/settings.py
+++ freechatbot/settings.py
@@ -88,7 +88,8 @@ WSGI_APPLICATION = 'freechatbot.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db_full.sqlite3'),
     }
 }
 
