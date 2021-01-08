6e1da18a436810133f2f7d7ff1d729f177a07f9b
wongfree@gmail.com <wongfree@gmail.com>
2021-01-08 15:06:32 +0800

    update train.py

--- train.py
+++ train.py
@@ -6,26 +6,69 @@ chatbot = ChatBot(**settings.CHATTERBOT)
 
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train(
-                "chatterbot.corpus.english",
-
+                "chatterbot.corpus.english.conversations",
+                "chatterbot.corpus.english.emotion",
+                "chatterbot.corpus.english.greetings",
+                "chatterbot.corpus.english.food",
+                "chatterbot.corpus.english.ai"
               )
 from chatterbot.trainers import ListTrainer
 
 trainer = ListTrainer(chatbot)
 
 trainer.train([
-    "How are you?",
-    "I am good.",
-    "That is good to hear.",
-    "Thank you",
-    "You are welcome.",
-    "What's your name?",
-    "My name is Chatterbot",
-    "know your name",
-    "My name is Chatterbot",
-    ])
+        "How are you?",
+        "I am good.",
+        "That is good to hear.",
+        "Thank you",
+        "You are welcome.",
+        "What's your name?",
+        "My name is Freechatbot",
+        "know your name",
+        "My name is Freechatbot",
+        ])
 
-from chatterbot.trainers import UbuntuCorpusTrainer
+trainer.train([
+    "Hello",
+    "Hello, How can I help you",
+    "My PC dead",
+    "Sorry, i Can't understand clearly, Can you further explain the symptom?",
+])
+
+trainer.train([
+    "Hi",
+    "Hello, How can I help you",
+    "My PC not working",
+    "Sorry, i Can't understand clearly, Can you further explain the symptom?",
+])
+
+trainer.train([
+    "Hi",
+    "Hello, How can I help you",
+    "My PC not working",
+    "Sorry, i Can't understand clearly, Can you further explain the symptom?",
+    "When i tried to open my excel, then it friezed",
+    "Have you try restart your machine?",
+    "Ok, let me try.",
+    "Anything else i can help?"
+])
+
+trainer.train([
+    "My program keep crashing",
+    "Which program you talking?",
+    "GIMP",
+    "Have you try restart your machine?",
+    "Yes, still same",
+    "Could you provide which version of the program your using?",
+    "2.1.1",
+    "Have you try update your program version?",
+    "Ok, let me try.",
+    "Anything else i can help?",
+    "No Thanks",
+    "Thank you very much for using ur services."
+])
+
+# from chatterbot.trainers import UbuntuCorpusTrainer
 
 
 # trainer = UbuntuCorpusTrainer(chatbot)
